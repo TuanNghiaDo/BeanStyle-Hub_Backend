f7f1a8e5096c373ef4cd40a78004cd8c85563b21
TuanNghiaDo <dotuannghia07@gmail.com>
2025-08-01 21:15:57 +0700

    Init project

---
+++ BeanStyle-Hub/src/main/java/com/beanjoseph/BeanStyle_Hub/BeanStyleHubApplication.java
@@ -0,0 +1,13 @@
+package com.beanjoseph.BeanStyle_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanStyleHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanStyleHubApplication.class, args);
+	}
+
+}
---
+++ BeanStyle-Hub/src/test/java/com/beanjoseph/BeanStyle_Hub/BeanStyleHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanjoseph.BeanStyle_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanStyleHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
